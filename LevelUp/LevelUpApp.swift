3bc86510b19e22710b44c83cd88e3d64d3de3270
aravela04 <aaron_rvl@outlook.com>
2026-03-08 20:44:23 -0500

    Initial Commit

---
+++ LevelUp/LevelUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  LevelUpApp.swift
+//  LevelUp
+//
+//  Created by Aaron Ravela on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct LevelUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
